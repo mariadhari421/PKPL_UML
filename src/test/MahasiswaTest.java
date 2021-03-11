faa9a988b18c7102ff043aa6c04ed66af4488765
mariadhari421 <mariadhari5@gmail.com>
2021-03-11 22:04:55 +0700

    test class mahasiswa

---
+++ src/test/MahasiswaTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import model.Mahasiswa;
+
+public class MahasiswaTest {
+    public static Mahasiswa mahasiswa;
+    public static void main(String[] args) {
+        
+    }
+    public static void testCase1() {
+        /**
+         *  Membuat instansiasi objek 
+         *  dosen dengan parameter string kosong
+         */
+        mahasiswa = new Mahasiswa("", "");
+    }
+}
